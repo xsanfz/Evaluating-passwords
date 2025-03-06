b4b2f623f21d2d3b48ea91b633cf3deb5cc8579e
xsanfz <sergey.myamin@gmail.com>
2025-03-06 10:59:39 +0400

    Add files via upload

--- main.py
+++ main.py
@@ -1,68 +1,37 @@
 password = input("Введите пароль: ")
 
 def has_digit(password):
-    for char in password:
-        if char.isdigit():
-            return True
-    return False
-
+    return any(char.isdigit() for char in password)
 
 def has_letters(password):
-    for char in password:
-        if char.isalpha():
-            return True
-    return False
-
+    return any(char.isalpha() for char in password)
 
 def is_very_long(password):
     return len(password) >= 12
 
-
-def calculate_password_score(password):
-    score = 0
-    if len(password) <= 8:
-        score = 0
-    else:
-        score += (len(password) - 8) / 2
-    return score
-
-
 def has_lower_letters(password):
-    for char in password:
-        if char.islower():
-            return True
-    return False
+    return any(char.islower() for char in password)
 
+def has_upper_letters(password):
+    return any(char.isupper() for char in password)
+
+def has_symbols(password):
+    special_chars = "!@#$%^&*()_+-=[]{}|;:,.<>?/"
+    return any(char in special_chars for char in password)
 
 def calculate_password_score(password):
-    score = 0
-    length = len(password)
+    checks = [
+        has_digit,
+        has_upper_letters,
+        has_lower_letters,
+        is_very_long,
+        has_symbols,
+    ]
 
-    # Учитываем длину пароля
-    if length <= 8:
-        score = 0
-    else:
-        score += (length - 8) / 2
+    score = max((len(password) - 8) / 2, 0)
 
-    # Учитываем наличие цифр
-    if has_digit(password):
-        score += 2
+    score += sum(2 for check in checks if check(password))
 
-    # Учитываем наличие заглавных букв
-    if has_upper_letters(password):
-        score += 2
+    return min(score, 10)
 
-    # Учитываем наличие строчных букв
-    if has_lower_letters(password):
-        score += 2
-
-    # Учитываем очень длинный пароль
-    if is_very_long(password):
-        score += 4
-
-    # Ограничиваем максимальный балл 8
-    return min(score, 8)
-
-
-#print(has_lower_letters()) # True
-print("Счет пароля:", calculate_password_score(password))
+print("Рейтинг пароля:", calculate_password_score(password))
