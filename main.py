6b7dc9125d8ff19fc5d52bfb02eda456ab41ef43
xsanfz <sergey.myamin@gmail.com>
2025-03-10 11:58:02 +0400

    Add files via upload

--- main.py
+++ main.py
@@ -1,20 +1,26 @@
 def has_digit(password):
     return any(char.isdigit() for char in password)
 
+
 def has_letters(password):
     return any(char.isalpha() for char in password)
+
 
 def is_very_long(password):
     return len(password) >= 12
 
+
 def has_lower_letters(password):
     return any(char.islower() for char in password)
+
 
 def has_upper_letters(password):
     return any(char.isupper() for char in password)
 
+
 def has_symbols(password):
     return any(not char.isalnum() for char in password)
+
 
 def calculate_password_score(password):
     checks = [
@@ -31,10 +37,12 @@ def calculate_password_score(password):
 
     return score
 
+
 def main():
     password = input("Введите пароль: ")
     score = calculate_password_score(password)
     print("Рейтинг пароля:", score)
+
 
 if __name__ == "__main__":
     main()
